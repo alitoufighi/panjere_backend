5e4ecbfa8663a7dbf4268c75f17aa125f28e97ff
felagund <zangeneh.saeed411@yahoo.com>
2018-12-27 11:27:54 +0330

    a

--- register/urls.py
+++ register/urls.py
@@ -6,5 +6,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('panjere/register', views.accept),
-    path('panjere', TemplateView.as_view(template_name='home.html')),
+    path('panjere/', TemplateView.as_view(template_name='home.html')),
 ]
